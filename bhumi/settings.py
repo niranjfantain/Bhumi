2db0ee4ef649e887a9736b2d4991babf7a9aaa0f
Niranj Rajasekaran <niranj@fantain.com>
2014-10-11 20:07:35 +0530

    Please Use pip install -r requirements.txt.Added Boostrap for Django Admin.

--- bhumi/settings.py
+++ bhumi/settings.py
@@ -30,13 +30,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django_admin_bootstrapped.bootstrap3',
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nakshatrams'
+    'nakshatrams',
 )
 
 MIDDLEWARE_CLASSES = (
