5a8698c3d65e34afa38fac62a0bf01125d9fc904
unknown <vivthangathurai@.corp.ebay.com>
2014-10-11 22:20:04 +0530

    Added Models for org. and participants with error.;:

--- nakshatra/admin.py
+++ nakshatra/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from nakshatra.models import Event, MajorCategory, MinorCategory
+from nakshatra.models import Event, MajorCategory, MinorCategory, Organization, Participant,EventEnrollment, City
 
 # Register your models here.
 
 admin.site.register(Event)
 admin.site.register(MajorCategory)
 admin.site.register(MinorCategory)
+admin.site.register(Organization)
+admin.site.register(Participant)
+admin.site.register(EventEnrollment)
+admin.site.register(City)
+
--- nakshatra/models.py
+++ nakshatra/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from bsddb.test.test_all import verbose
 
 # Create your models here.
 
@@ -12,13 +13,25 @@ EVENT_TYPE = (
     ('G', 'Group'),
 )
 
-
+CATEGORY_LEVEL = (
+    ('S', 'Senior'),
+    ('SS', 'Super Senior'),
+    ('J', 'Junior'),
+)
 class MajorCategory(models.Model):
     name = models.CharField(max_length=50, verbose_name="Major Category Name")
+    
+    class Meta:
+        verbose_name = "MajorCategory"
+        verbose_name_plural = "MajorCategories"
 
 
 class MinorCategory(models.Model):
     name = models.CharField(max_length=50, verbose_name="Minor Category Name")
+    
+    class Meta:
+        verbose_name = "MinorCategory"
+        verbose_name_plural = "MinorCategories"
 
 
 class Location(models.Model):
@@ -31,7 +44,36 @@ class City(models.Model):
         Location,
         verbose_name="Location Name"
     )
+    
+    class Meta:
+        verbose_name = "City"
+        verbose_name_plural = "Cities"
 
+class Organization(models.Model):
+    name = models.CharField(max_length=75, verbose_name="Organization Name")
+    location = models.ForeignKey(
+        Location,
+        verbose_name="Location Name"
+    )
+    
+class Participant(models.Model):
+    name = models.CharField(max_length=75, verbose_name="Participant Name")
+    dob = models.DateField(verbose_name="Date Of Birth")
+    age = models.IntegerField(verbose_name="Age")
+    gender = models.CharField(
+        max_length=2,
+        choices=GENDER,
+        default='M',
+        verbose_name='Gender'
+    )
+    interest = models.TextField(max_length=150, verbose_name="Participant Interest")
+    category = models.CharField(
+        max_length=2,
+        choices=CATEGORY_LEVEL,
+        default='J',
+        verbose_name='Category Level'
+    )
+        
 
 class Event(models.Model):
     eventName = models.CharField(max_length=50, verbose_name="Event Name")
@@ -63,3 +105,14 @@ class Event(models.Model):
     )
     minP = models.IntegerField(default=None, verbose_name="Minimum Participant")
     maxP = models.IntegerField(default=None, verbose_name="Maximum Participant")
+    
+class EventEnrollment(models.Model):
+    event = models.ForeignKey(
+        Event,
+        verbose_name="Event"
+    )
+    participants = models.ForeignKey(
+        Participant,
+        verbose_name="Minor Category"
+    )
+
