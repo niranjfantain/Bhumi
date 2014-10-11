ee32ee99ea9995e50b219651b8481435708ba8a2
Niranj Rajasekaran <niranj@fantain.com>
2014-10-11 16:44:22 +0530

    Added models for event

--- bhumi/nakshatrams/models.py
+++ bhumi/nakshatrams/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+GENDER = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+)
+
+EVENT_TYPE = (
+    ('I', 'Indivigual'),
+    ('G', 'Group'),
+)
+
+
+class MajorCategory(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Major Category Name")
+
+
+class MinorCategory(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Minor Category Name")
+
+
+class Event(models.Model):
+    eventName = models.CharField(max_length=50, verbose_name="Event Name")
+    desc = models.TextField(max_length=150, verbose_name="Description")
+    majorCategory = models.ForeignKey(
+        MajorCategory,
+        verbose_name="Major Category"
+    )
+    minorCategory = models.ForeignKey(
+        MinorCategory,
+        verbose_name="Minor Category"
+    )
+    eventType = models.CharField(
+        choices=EVENT_TYPE,
+        default='I',
+        verbose_name='Event Type'
+    )
+    gender = models.CharField(
+        choices=GENDER,
+        default='M',
+        verbose_name='Gender'
+    )
