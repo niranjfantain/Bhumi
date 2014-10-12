fd26bd1bd31bcf70ffb39def5d547187b1101bf0
unknown <vivthangathurai@.corp.ebay.com>
2014-10-12 09:48:17 +0530

    added report functionality.

--- bhumi/urls.py
+++ bhumi/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^report_builder/', include('report_builder.urls')),
 )
